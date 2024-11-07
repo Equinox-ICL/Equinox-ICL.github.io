efd0256b4fee9941deacdbd58bcbc20ef58bb20e
GabrielSalt <salt.gabriel@gmail.com>
2024-11-07 19:34:49 +0000

    new stuff on about.html

--- static/js/togglemenu.js
+++ static/js/togglemenu.js
@@ -1,8 +1,10 @@
 function toggleMenu() {
     const menu = document.getElementById('dropdown-menu');
-    if (menu.style.display === 'flex') {
+    if (menu && menu.style.display === 'flex') {
         menu.style.display = 'none';
     } else {
         menu.style.display = 'flex';
     }
 }
+
+document.addEventListener('DOMContentLoaded', toggleMenu);
