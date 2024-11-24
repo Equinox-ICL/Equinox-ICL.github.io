080f826b42e69bd1bf3ac9ef74db6b23ab59b78a
GabrielSalt <salt.gabriel@gmail.com>
2024-11-24 16:46:37 +0000

    Updates made to about, connect and projects

--- static/js/togglemenu.js
+++ static/js/togglemenu.js
@@ -1,10 +1,21 @@
-function toggleMenu() {
-    const menu = document.getElementById('dropdown-menu');
-    if (menu && menu.style.display === 'flex') {
-        menu.style.display = 'none';
-    } else {
-        menu.style.display = 'flex';
-    }
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-document.addEventListener('DOMContentLoaded', toggleMenu);
+async function highlightMenu() {
+    await sleep(100);
+    const menu = document.getElementById("headerList");
+    for (const child of menu.children) {
+        console.log(window.location.pathname)
+        if(window.location.pathname == "/" &&  child.name == "home"){
+            child.classList.add("rust")
+            break
+        }
+        if (window.location.pathname.includes(child.name)){
+            child.classList.add("rust")
+            break;
+        }
+    }
+    
+}
+document.addEventListener('DOMContentLoaded', highlightMenu);
